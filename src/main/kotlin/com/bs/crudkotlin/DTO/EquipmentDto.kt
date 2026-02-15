da0430a4c901ed6bf7a6fba831de9dc9615a43de
gimhan-yeong <hanyung0223@gmail.com>
2026-02-16 03:28:21 +0900

    dto -> entity 변환 함수 startdate 변수 삭제

--- src/main/kotlin/com/bs/crudkotlin/DTO/EquipmentDto.kt
+++ src/main/kotlin/com/bs/crudkotlin/DTO/EquipmentDto.kt
@@ -3,14 +3,15 @@ package com.bs.crudkotlin.DTO
 import com.bs.crudkotlin.Entity.EquipmentEntity
 import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDate
-import java.time.LocalDateTime
 
 data class EquipmentDto(
     val num:Long?,
     val name:String,
     val status:String,
     @JsonFormat(pattern = "yyyy-MM-dd")
-    val deadline:LocalDate?
+    val deadline:LocalDate?,
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    val startdate: LocalDate?
 ){  // Dto -> Entity 변환
     fun toEntity() = EquipmentEntity(
         num=num,
@@ -21,10 +22,12 @@ data class EquipmentDto(
     // Entity -> Dto 변환
     companion object {
         fun fromEntity(entity: EquipmentEntity) = EquipmentDto(
-            num = entity.num,
-            name = entity.name,
-            status = entity.status,
-            deadline = entity.deadline
+            num=entity.num,
+            name=entity.name,
+            status=entity.status,
+            deadline=entity.deadline,
+            startdate=entity.startdate
+
         )
     }
 }
