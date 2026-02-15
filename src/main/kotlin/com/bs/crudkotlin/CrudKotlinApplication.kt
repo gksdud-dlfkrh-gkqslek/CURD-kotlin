ccbe7440122d19b923be4b2fc8b15629cf1605a0
gimhan-yeong <hanyung0223@gmail.com>
2026-02-16 03:31:49 +0900

    @EnableJpaAuditing 추가

--- src/main/kotlin/com/bs/crudkotlin/CrudKotlinApplication.kt
+++ src/main/kotlin/com/bs/crudkotlin/CrudKotlinApplication.kt
@@ -2,8 +2,10 @@ package com.bs.crudkotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class CrudKotlinApplication
 
 fun main(args: Array<String>) {
