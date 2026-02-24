a572c727c5a4ad6ff6f0f4f228c6cabaa8b8d217
gimhan-yeong <hanyung0223@gmail.com>
2026-02-25 02:32:40 +0900

    UserRepository.kt 작성

--- src/main/kotlin/com/bs/crudkotlin/Repository/UserRepository.kt
+++ src/main/kotlin/com/bs/crudkotlin/Repository/UserRepository.kt
@@ -1,4 +1,11 @@
 package com.bs.crudkotlin.Repository
 
-class UserRepository {
+import com.bs.crudkotlin.Entity.UserEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : JpaRepository<UserEntity, String> {
+    fun findByPhone(phone: Long): UserEntity?
+    fun existsByPhone(phone: Long): Boolean // 중복 체크
 }
