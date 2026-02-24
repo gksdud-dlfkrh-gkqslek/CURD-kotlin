8f91ff1167b0f3f52105acf8f9a9bd06c0f2188c
gimhan-yeong <hanyung0223@gmail.com>
2026-02-25 02:26:14 +0900

    UserEntity.kt 작성

--- src/main/kotlin/com/bs/crudkotlin/Entity/UserEntity.kt
+++ src/main/kotlin/com/bs/crudkotlin/Entity/UserEntity.kt
@@ -1,4 +1,36 @@
 package com.bs.crudkotlin.Entity
 
-class UserEntity {
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.hibernate.annotations.processing.Pattern
+import java.util.UUID
+
+@Entity
+@Table(name = "users")
+class UserEntity(
+    @Id
+    @Column(length = 36)
+    val id: String = UUID.randomUUID().toString(),
+
+    @Column(nullable = false, unique = true)
+    var phone: Long,
+
+    @Column(nullable = false)
+    var password: String,  // 암호화된 비밀번호 저장
+
+    @Column(nullable = false, length = 50)
+    var name: String,
+
+    @Column(nullable = false, length = 20)
+    @Enumerated(EnumType.STRING)
+    var role: UserRole = UserRole.USER,
+
+)
+enum class UserRole {
+    USER,   // 일반 사용자
+    ADMIN   // 관리자
 }
