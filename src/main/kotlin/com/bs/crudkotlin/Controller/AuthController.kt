ed50207738f13b33a5a10d829b4184816b5e1e88
gimhan-yeong <hanyung0223@gmail.com>
2026-02-25 22:12:20 +0900

    로그인 controller 설정

--- src/main/kotlin/com/bs/crudkotlin/Controller/AuthController.kt
+++ src/main/kotlin/com/bs/crudkotlin/Controller/AuthController.kt
@@ -1,10 +1,12 @@
 package com.bs.crudkotlin.Controller
 
+import com.bs.crudkotlin.DTO.LoginRequest
 import com.bs.crudkotlin.DTO.SignUpRequest
 import com.bs.crudkotlin.DTO.UserResponse
 import com.bs.crudkotlin.Entity.UserEntity
 import com.bs.crudkotlin.Repository.UserRepository
 import com.bs.crudkotlin.Service.AuthService
+import jakarta.servlet.http.HttpSession
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -20,6 +22,15 @@ class AuthController(private val authService: AuthService) {
     @PostMapping("/signup")
     fun signUp(@RequestBody request: SignUpRequest): ResponseEntity<UserResponse?>? {
         val user = authService.signUp(request)
+        return ResponseEntity.ok(user)
+    }
+
+    //로그인
+    @PostMapping("/login")
+    fun login(
+        @RequestBody request: LoginRequest,
+        session: HttpSession): ResponseEntity<UserResponse> {
+        val user = authService.login(request, session)
         return ResponseEntity.ok(user)
     }
 }
