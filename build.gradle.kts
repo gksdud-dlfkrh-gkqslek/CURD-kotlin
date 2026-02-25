0b555b6a92837574027aa7f8f397d1ab4d199655
gimhan-yeong <hanyung0223@gmail.com>
2026-02-25 21:58:28 +0900

    security 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 }
 
 kotlin {
