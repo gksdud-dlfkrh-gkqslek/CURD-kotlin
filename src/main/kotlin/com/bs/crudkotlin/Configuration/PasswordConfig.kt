86759c3ab0f5afa5062c9878ecba5c9551b6fd7d
gimhan-yeong <hanyung0223@gmail.com>
2026-02-25 21:13:56 +0900

    PasswordConfig.kt 작성

--- src/main/kotlin/com/bs/crudkotlin/Configuration/PasswordConfig.kt
+++ src/main/kotlin/com/bs/crudkotlin/Configuration/PasswordConfig.kt
@@ -1,4 +1,14 @@
 package com.bs.crudkotlin.Configuration
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
 class PasswordConfig {
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
 }
