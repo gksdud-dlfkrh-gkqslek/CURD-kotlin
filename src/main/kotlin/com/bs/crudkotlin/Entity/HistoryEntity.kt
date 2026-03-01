dd982c113b9d2c0d3bd80799008b2eb5cd63b1b2
gimhan-yeong <hanyung0223@gmail.com>
2026-03-02 03:34:31 +0900

    저장될 정보

--- src/main/kotlin/com/bs/crudkotlin/Entity/HistoryEntity.kt
+++ src/main/kotlin/com/bs/crudkotlin/Entity/HistoryEntity.kt
@@ -1,9 +1,43 @@
 package com.bs.crudkotlin.Entity
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.Id
 import jakarta.persistence.Table
+import java.time.LocalDate
+import java.util.UUID
 
 @Entity
 @Table(name = "history")
-class HistoryEntity {
-}
+class HistoryEntity(
+    @Id
+    @Column(length = 36)
+    val id: String = UUID.randomUUID().toString(),
+
+    @Column(nullable = false)
+    val equipmentId: String,
+
+    @Column(nullable = false)
+    val equipmentName: String,
+
+    @Column(nullable = false)
+    val userId: String,
+
+    @Column(nullable = false)
+    val userName: String,
+
+    @Column(nullable = false)
+    val userPhone: String,
+
+    @Column(nullable = true)
+    val reservedDate: LocalDate? = null,
+
+    @Column(nullable = true)
+    val deadline: LocalDate? = null,
+
+    @Column(nullable = false)
+    val returnDate: LocalDate = LocalDate.now(),
+
+    @Column(nullable = false)
+    val returnStatus: String
+)
