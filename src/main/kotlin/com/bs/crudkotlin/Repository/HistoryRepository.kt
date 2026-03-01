9fc4d63a056350818b057a193ab986d7a97540d1
gimhan-yeong <hanyung0223@gmail.com>
2026-03-02 03:36:34 +0900

    HistoryRepository.kt 파일 생성

---
+++ src/main/kotlin/com/bs/crudkotlin/Repository/HistoryRepository.kt
@@ -0,0 +1,8 @@
+package com.bs.crudkotlin.Repository
+
+import com.bs.crudkotlin.Entity.HistoryEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface HistoryRepository : JpaRepository<HistoryEntity, String> {}
