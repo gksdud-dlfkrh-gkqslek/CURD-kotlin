b119e78a18e149970e477d9cf030ad6e627a0772
gimhan-yeong <hanyung0223@gmail.com>
2026-03-03 02:22:39 +0900

    UnnumberedEquipmentController.kt 파일 생성

---
+++ src/main/kotlin/com/bs/crudkotlin/Controller/UnnumberedEquipmentController.kt
@@ -0,0 +1,15 @@
+package com.bs.crudkotlin.Controller
+
+import org.springframework.stereotype.Controller
+
+@Controller
+class UnnumberedEquipmentController {
+    //번호 없는 장비 등록
+    //번호 없는 장비 삭제
+    //번호 없는 장비 조회
+    //번호 없는 장비 재고 수정
+    //번호 없는 장비 예약
+    //번호 없는 장비 반납 요청
+    //번호 없는 장비 반납 승인
+    //번호 없는 장비 반납 대기 목록
+}
