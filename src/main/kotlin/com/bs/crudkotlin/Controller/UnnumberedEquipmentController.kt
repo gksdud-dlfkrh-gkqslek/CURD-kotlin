e88f44bd49dd8996118c4c35b7cb0e97abc8df19
gimhan-yeong <hanyung0223@gmail.com>
2026-03-03 03:12:13 +0900

    번호 없는 장비 삭제 기능 추가

--- src/main/kotlin/com/bs/crudkotlin/Controller/UnnumberedEquipmentController.kt
+++ src/main/kotlin/com/bs/crudkotlin/Controller/UnnumberedEquipmentController.kt
@@ -5,7 +5,9 @@ import com.bs.crudkotlin.DTO.UnnumberedEquipmentDto
 import com.bs.crudkotlin.Service.UnnumberedEquipmentService
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -21,8 +23,12 @@ class UnnumberedEquipmentController(
         return unnumberedEquipmentService.unnumbercreate(unnumberedEquipmentDto)
     }
 
-
     //번호 없는 장비 삭제
+    @DeleteMapping("/{id}")
+    fun unnumbereddelete(@PathVariable id:String):String{
+        return unnumberedEquipmentService.unnumberdelete(id)
+    }
+
     //번호 없는 장비 조회
     //번호 없는 장비 재고 수정
     //번호 없는 장비 예약
--- src/main/kotlin/com/bs/crudkotlin/Service/UnnumberedEquipmentService.kt
+++ src/main/kotlin/com/bs/crudkotlin/Service/UnnumberedEquipmentService.kt
@@ -12,9 +12,18 @@ import org.springframework.web.bind.annotation.RequestBody
 class UnnumberedEquipmentService(
     private val unnumberedEquipmentRepository: UnnumberedEquipmentRepository,
 ) {
+    //번호 없는 장비 등록
     fun unnumbercreate(unnumberedEquipmentDto: UnnumberedEquipmentDto):String{
         val entity: UnnumberedEquipmentEntity = unnumberedEquipmentDto.toEntity()
         unnumberedEquipmentRepository.save(entity)
         return "저장 완료"
     }
+
+    //번호 없는 장비 삭제
+    fun unnumberdelete(id:String):String{
+        val entity: UnnumberedEquipmentEntity = unnumberedEquipmentRepository.findById(id).orElse(null)
+        unnumberedEquipmentRepository.delete(entity)
+        return "삭제 완료"
+    }
+
 }
