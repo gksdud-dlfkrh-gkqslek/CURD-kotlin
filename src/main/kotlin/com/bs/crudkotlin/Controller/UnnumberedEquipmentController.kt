26a263916c9018c3b773640e951689ec0ab3c36c
gimhan-yeong <hanyung0223@gmail.com>
2026-03-03 03:06:50 +0900

    번호 없는 장비 등록 기능 추가

--- src/main/kotlin/com/bs/crudkotlin/Controller/UnnumberedEquipmentController.kt
+++ src/main/kotlin/com/bs/crudkotlin/Controller/UnnumberedEquipmentController.kt
@@ -1,10 +1,27 @@
 package com.bs.crudkotlin.Controller
 
+import com.bs.crudkotlin.DTO.EquipmentDto
+import com.bs.crudkotlin.DTO.UnnumberedEquipmentDto
+import com.bs.crudkotlin.Service.UnnumberedEquipmentService
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-class UnnumberedEquipmentController {
+@RequestMapping("/api/unnumbered")
+class UnnumberedEquipmentController(
+    private val unnumberedEquipmentService: UnnumberedEquipmentService
+) {
     //번호 없는 장비 등록
+    @PostMapping
+    fun unnumbercreate(@RequestBody unnumberedEquipmentDto: UnnumberedEquipmentDto):String{
+        return unnumberedEquipmentService.unnumbercreate(unnumberedEquipmentDto)
+    }
+
+
     //번호 없는 장비 삭제
     //번호 없는 장비 조회
     //번호 없는 장비 재고 수정
--- src/main/kotlin/com/bs/crudkotlin/Service/UnnumberedEquipmentService.kt
+++ src/main/kotlin/com/bs/crudkotlin/Service/UnnumberedEquipmentService.kt
@@ -1,7 +1,20 @@
 package com.bs.crudkotlin.Service
 
+import com.bs.crudkotlin.DTO.EquipmentDto
+import com.bs.crudkotlin.DTO.UnnumberedEquipmentDto
+import com.bs.crudkotlin.Entity.UnnumberedEquipmentEntity
+import com.bs.crudkotlin.Repository.UnnumberedEquipmentRepository
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.RequestBody
 
 @Service
-class UnnumberedEquipmentService {
+class UnnumberedEquipmentService(
+    private val unnumberedEquipmentRepository: UnnumberedEquipmentRepository,
+) {
+    fun unnumbercreate(unnumberedEquipmentDto: UnnumberedEquipmentDto):String{
+        val entity: UnnumberedEquipmentEntity = unnumberedEquipmentDto.toEntity()
+        unnumberedEquipmentRepository.save(entity)
+        return "저장 완료"
+    }
 }
