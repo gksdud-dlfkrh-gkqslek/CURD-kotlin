7bbac39498470dee3ae7c8e01f1b4c52d65f47c4
gimhan-yeong <hanyung0223@gmail.com>
2026-03-04 04:08:07 +0900

    UnnumberedReservationRepository.kt 파일 생성

---
+++ src/main/kotlin/com/bs/crudkotlin/Repository/UnnumberedReservationRepository.kt
@@ -0,0 +1,8 @@
+package com.bs.crudkotlin.Repository
+
+import com.bs.crudkotlin.Entity.UnnumberedEquipmentEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UnnumberedReservationRepository
