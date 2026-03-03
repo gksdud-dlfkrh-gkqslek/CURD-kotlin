a2aa1d8a9bb1f143976f34d44ede0a9dc2a8d901
gimhan-yeong <hanyung0223@gmail.com>
2026-03-04 04:26:01 +0900

    UnnumberedReservationRepository.kt jpa 추가

--- src/main/kotlin/com/bs/crudkotlin/Repository/UnnumberedReservationRepository.kt
+++ src/main/kotlin/com/bs/crudkotlin/Repository/UnnumberedReservationRepository.kt
@@ -1,8 +1,8 @@
 package com.bs.crudkotlin.Repository
 
-import com.bs.crudkotlin.Entity.UnnumberedEquipmentEntity
+import com.bs.crudkotlin.Entity.UnnumberedReservationEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UnnumberedReservationRepository
+interface UnnumberedReservationRepository : JpaRepository<UnnumberedReservationEntity, String>
