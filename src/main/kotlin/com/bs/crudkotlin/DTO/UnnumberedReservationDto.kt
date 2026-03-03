7c58cb4a3a9e8ef7962a18f001e10f98fc61e4a5
gimhan-yeong <hanyung0223@gmail.com>
2026-03-04 03:26:43 +0900

    번호 없는 장비 예약 dto, entity 생성

---
+++ src/main/kotlin/com/bs/crudkotlin/DTO/UnnumberedReservationDto.kt
@@ -0,0 +1,44 @@
+package com.bs.crudkotlin.DTO
+
+import com.bs.crudkotlin.Entity.UnnumberedEquipmentEntity
+import com.bs.crudkotlin.Entity.UnnumberedReservationEntity
+import com.fasterxml.jackson.annotation.JsonFormat
+import jakarta.persistence.Column
+import java.time.LocalDate
+import java.util.UUID
+
+data class UnnumberedReservationDto(
+
+    val id: String? = null,
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    var deadline: LocalDate?,
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    var startdate: LocalDate? = null,
+
+    var userId: String? = null,
+
+    var returnStatus: String,
+
+    var returnPending: Boolean = false
+) {
+    //DTO -> Entity
+    fun toEntity() = UnnumberedReservationEntity(
+        deadline = deadline,
+        startdate = startdate,
+        returnStatus = returnStatus
+    )
+
+    //Entity -> DTO
+    companion object {
+        fun fromEntity(entity: UnnumberedReservationEntity) = UnnumberedReservationDto(
+            id = entity.id,
+            deadline = entity.deadline,
+            startdate = entity.startdate,
+            userId = entity.userId,
+            returnStatus = entity.returnStatus,
+            returnPending = entity.returnPending
+        )
+    }
+}
---
+++ src/main/kotlin/com/bs/crudkotlin/Entity/UnnumberedReservationEntity.kt
@@ -0,0 +1,31 @@
+package com.bs.crudkotlin.Entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.time.LocalDate
+import java.util.UUID
+
+@Entity
+@Table(name = "unnumbered_reservation")
+class UnnumberedReservationEntity (
+    @Id
+    @Column(length = 36)
+    val id: String = UUID.randomUUID().toString(),
+
+    @Column(nullable = true)
+    var deadline: LocalDate? = null,
+
+    @Column(nullable = true)
+    var startdate: LocalDate? = null,
+
+    @Column(nullable = true)
+    var userId: String? = null,
+
+    @Column(nullable = false, length = 30)
+    var returnStatus: String,
+
+    @Column(nullable = false)
+    var returnPending: Boolean = false
+)
